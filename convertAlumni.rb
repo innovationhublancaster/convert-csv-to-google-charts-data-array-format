39a58cfca3b46c2bacdf3554db78004771f346a3
Josh <josh@dyn-135-235.lancs.ac.uk>
2015-07-01 16:04:59 +0100

    initial commit - turns COUNTRY, INT into the format : "['Country', INT]" - and repeats this for each row of the csv file.

---
+++ convertAlumni.rb
@@ -0,0 +1,10 @@
+require 'csv'
+require 'time'
+CSV.foreach("alumni.csv") do |row|
+  puts(row[0] , row[1])
+
+  CSV.open(Time.now.to_s + "newAlumni.csv", "a+", {force_quotes: false}) do |csv|
+    var = "['" + row[0].capitalize + "', " + row[1] + "],"
+    csv << [var]
+  end
+end
